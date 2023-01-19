63c099d621b41264f06b0834f2bd36a80570b4e1
Amol shelke <shelkeamol897@gmail.com>
2023-01-19 23:32:44 +0530

    Added back the hamburger animation

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-// init
-// const menuIcon = document.querySelector("#menu-icon");
-// const navMenu = document.querySelector(".navmenu");
+init;
+const menuIcon = document.querySelector("#menu-icon");
+const navMenu = document.querySelector(".navmenu");
 
-// menuIcon.addEventListener("click", () => {
-//   navMenu.classList.toggle("open");
-// });
-
-let str1 = "Amol";
-let str2 = "Amol";
-
-const countNum = 20;
-
-for (let i = 0; i < countNum; i++) {
-  console.log("Hello i am for loop");
-}
-
-function printMyAge(birthYear, currentYear) {
-  console.log(currentYear - birthYear);
-}
-
-printMyAge(2004, 2023);
+menuIcon.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+});
