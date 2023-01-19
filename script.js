66d193fa4ddebacd298629f1b00518201d8316d0
Amol shelke <shelkeamol897@gmail.com>
2023-01-19 23:32:14 +0530

    Adding animation with JS

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // init
-const menuIcon = document.querySelector("#menu-icon");
-const navMenu = document.querySelector(".navmenu");
+// const menuIcon = document.querySelector("#menu-icon");
+// const navMenu = document.querySelector(".navmenu");
 
-menuIcon.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
-});
+// menuIcon.addEventListener("click", () => {
+//   navMenu.classList.toggle("open");
+// });
+
+let str1 = "Amol";
+let str2 = "Amol";
+
+const countNum = 20;
+
+for (let i = 0; i < countNum; i++) {
+  console.log("Hello i am for loop");
+}
+
+function printMyAge(birthYear, currentYear) {
+  console.log(currentYear - birthYear);
+}
+
+printMyAge(2004, 2023);
